084db5e4983e271b8142604464a40e7872d1f8ed
shreevallabha-im <140231149+shreevallabha-im@users.noreply.github.com>
2023-11-13 22:09:45 +0530

    init

---
+++ cryptochain/pubsub.js
@@ -0,0 +1,58 @@
+const redis = require('redis');
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN'
+};
+
+class PubSub {
+  constructor({blockchain}) {
+    this.blockchain = blockchain
+
+    // Specify the host and port for your Redis server
+    this.publisher = redis.createClient({
+      host: 'www.projects.iotreeminds.com', // Replace with your actual host
+      port: 6379, // Replace with your actual port
+    });
+
+    this.subscriber = redis.createClient({
+      host: 'www.projects.iotreeminds.com', // Replace with your actual host
+      port: 6379, // Replace with your actual port
+    });
+
+    this.subscribeToChannels()
+
+    this.subscriber.on(
+      'message',
+      (channel, message) => this.handleMessage(channel, message)
+    );
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel=>{
+      this.subscriber.subscribe(channel)
+    })
+  }
+
+  publish({channel, message}) {
+    this.publisher.publish(channel, message)
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    })
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+    
+    const parsedMessage = JSON.parse(message)
+    if(channel===CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage)
+    }
+  }
+}
+
+module.exports = PubSub
